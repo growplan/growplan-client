3908240bd9c1fd94a5a336a957f8d93b56d429da
minhyeong <minhyeong.dev@gmail.com>
2024-11-29 15:31:13 +0900

    [Feat] create auth config

---
+++ src/config/auth.ts
@@ -0,0 +1,31 @@
+import { OAuthType } from "@/interface/OAuth";
+
+const {
+  VITE_OAUTH_NAVER_ORIGIN: naverOrigin,
+  VITE_OAUTH_NAVER_CLIENT_ID: naverClientId,
+  VITE_OAUTH_KAKAO_ORIGIN: kakaoOrigin,
+  VITE_OAUTH_KAKAO_CLIENT_ID: kakaoClientId,
+} = import.meta.env;
+
+const href = (provider: OAuthType) =>
+  [location.href, "auth", provider].join("/");
+const auth: Record<OAuthType, string> = {
+  naver: [
+    naverOrigin,
+    [
+      "response_type=code",
+      `client_id=${naverClientId}`,
+      `redirect_uri=${href("naver")}`,
+    ].join("&"),
+  ].join("?"),
+  kakao: [
+    kakaoOrigin,
+    [
+      "response_type=code",
+      `client_id=${kakaoClientId}`,
+      `redirect_uri=${href("kakao")}`,
+    ].join("&"),
+  ].join("?"),
+};
+
+export default auth;
