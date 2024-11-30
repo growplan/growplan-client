787cbe0863c0a3dccb63c2e2e92aea298b1d7c0d
minhyeong <minhyeong.dev@gmail.com>
2024-11-30 13:49:16 +0900

    [Feat] create ```checks```

---
+++ src/components/survey/check.ts
@@ -0,0 +1,22 @@
+interface Check {
+  id: number;
+  script: string;
+}
+export const checks: Check[] = [
+  {
+    id: 1,
+    script: "잘 할 수 있어요",
+  },
+  {
+    id: 2,
+    script: "할 수 있는 편이에요",
+  },
+  {
+    id: 3,
+    script: "하지 못하는 편이에요",
+  },
+  {
+    id: 4,
+    script: "전혀 할 수 없어요",
+  },
+];
