a109ee0e552fe3910934e972cfb63be96b9ce650
minhyeong <minhyeong.dev@gmail.com>
2024-11-30 13:49:48 +0900

    [Feat] add option

--- src/design/Card.tsx
+++ src/design/Card.tsx
@@ -3,17 +3,27 @@ import { Shelf } from "fast-jsx";
 
 interface CardProps {
   children: ReactNode;
+  option?: {
+    background?: string;
+    gap?: string;
+    boundary?: string;
+    pressure?: string;
+  };
 }
-export default function Card({ children }: CardProps) {
+export default function Card({ children, option }: CardProps) {
   return (
     <Shelf.Col
       option={{
-        display: "items-center",
+        display: [option?.gap, "items-center"].filter((v) => v).join(" "),
         width: "w-[353px]",
-        boundary:
-          "shadow-md shadow-[#6B7076]/50 rounded-[10px] overflow-hidden",
-        background: "bg-white",
-        pressure: "pt-7",
+        boundary: [
+          option?.boundary ?? "shadow-md shadow-[#6B7076]/50",
+          "rounded-[10px] overflow-hidden",
+        ]
+          .filter((v) => v)
+          .join(" "),
+        background: option?.background ?? "bg-white",
+        pressure: [option?.pressure, "pt-7"].filter((v) => v).join(" "),
       }}
     >
       {children}
