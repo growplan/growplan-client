42c903fb4b9ca9c1f2870f48f0d5d885df390631
minhyeong <minhyeong.dev@gmail.com>
2024-12-08 01:27:08 +0900

    [Test]

--- src/components/dashboard/root/molecule/CreateChildModal.molecule.tsx
+++ src/components/dashboard/root/molecule/CreateChildModal.molecule.tsx
@@ -1,18 +1,34 @@
+import { userApi } from "@/connection";
+import useSign from "@/hook/useSign";
 import { Input, Modal } from "fast-jsx";
 import { ShowAction } from "fast-jsx/interface/Action";
 import { useState } from "react";
+import { useMutation } from "react-query";
 
 export default function CreateChildModal({
   showAction,
 }: {
   showAction?: ShowAction;
 }) {
+  const { sign } = useSign();
   const [name, setName] = useState("");
   const [birthday, setBirthday] = useState("");
   const [gener, setGener] = useState("");
   const [bornHeight, setBornHeight] = useState("");
   const [bornWeight, setBornWeight] = useState("");
   const [isPremature, setIsPremature] = useState("");
+  const { mutate: postChild } = useMutation({
+    mutationKey: ["createChild"],
+    mutationFn: (userId: number) =>
+      userApi.child.post(userId, {
+        name: "김메디",
+        birthdate: "2020-06-07",
+        gender: "남자",
+        bornHeight: 49.9,
+        bornWeight: 3.3,
+        isPremature: false,
+      }),
+  });
   return (
     <Modal
       titles={{
@@ -37,6 +53,14 @@ export default function CreateChildModal({
         state={[bornWeight, setBornWeight]}
       />
       <Input placeholder="우량아여부" state={[isPremature, setIsPremature]} />
+      <button
+        onClick={() => {
+          if (!sign) return;
+          postChild(sign.id);
+        }}
+      >
+        등록
+      </button>
     </Modal>
   );
 }
