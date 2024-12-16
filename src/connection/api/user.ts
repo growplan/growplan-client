0f090adfdd8f6b07eb8625074723d56ffbdbb1a2
minhyeong <minhyeong.dev@gmail.com>
2024-12-16 14:27:13 +0900

    [Feat] feature to post Record

--- src/connection/api/user.ts
+++ src/connection/api/user.ts
@@ -1,6 +1,7 @@
 import { Child, CreateChild } from "@/interface/Child";
 import httpRequest from "../axios";
 import { calculateWeeksSince } from "@/util/calculate";
+import { CreateRecord, Record } from "@/interface/Record";
 
 const api = httpRequest.api();
 
@@ -22,10 +23,59 @@ async function postChild(
   return response.data;
 }
 
+async function getRecords(userId: number, childId: number) {
+  const response = await api.get<{ records: Record[] }>(
+    `/users/${userId}/childs/${childId}/records`
+  );
+  return response.data.records;
+}
+
+const postApi = httpRequest.api(undefined, {
+  contentType: "multipart/form-data",
+});
+
+async function postRecord({
+  userId,
+  childId,
+  createRecord,
+}: {
+  userId: number;
+  childId: number;
+  createRecord: CreateRecord;
+}) {
+  const formData = new FormData();
+  formData.append(
+    "requestDto",
+    new Blob(
+      [
+        JSON.stringify({
+          script: createRecord.requestDto.script,
+          developmentTypes: createRecord.requestDto.developmentTypes,
+        }),
+      ],
+      {
+        type: "application/json",
+      }
+    )
+  );
+  createRecord.files.forEach((file) => {
+    formData.append("files", file);
+  });
+  const response = await postApi.post(
+    `/users/${userId}/childs/${childId}/records`,
+    formData
+  );
+  return response.data;
+}
+
 const userApi = {
   child: {
     get: getChilds,
     post: postChild,
+  },
+  record: {
+    get: getRecords,
+    post: postRecord,
   },
 };
 export default userApi;
