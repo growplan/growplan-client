88994dfc8db13051cb359f6475b124a5c49f50ac
minhyeong <minhyeong.dev@gmail.com>
2024-12-16 14:24:40 +0900

    [Refactor] use type

--- src/components/dashboard/record/organism/Content.ts
+++ src/components/dashboard/record/organism/Content.ts
@@ -1,15 +1,15 @@
-import { DevelopmentTermType } from "@/interface/Development";
+import { DevelopmentTermType, DevelopmentType } from "@/interface/Development";
 
-interface Content {
+export interface Content {
   title: string;
-  type: DevelopmentTermType | "all";
+  type: DevelopmentType | "all";
 }
 export const contents: Content[] = [
   { title: "전체", type: "all" },
-  { title: "대근육", type: "globalMuscle" },
-  { title: "소근육", type: "localMuscle" },
-  { title: "인지", type: "cognitive" },
-  { title: "언어", type: "language" },
-  { title: "사회성", type: "social" },
-  { title: "자조", type: "selfHelp" },
+  { title: "대근육", type: "GM" },
+  { title: "소근육", type: "LM" },
+  { title: "인지", type: "CG" },
+  { title: "언어", type: "LG" },
+  { title: "사회성", type: "SC" },
+  { title: "자조", type: "SH" },
 ];
