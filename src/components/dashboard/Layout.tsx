97e6fa0b7b195fcfdad68495a21c9405c7f2a1ea
minhyeong <minhyeong.dev@gmail.com>
2024-12-19 12:58:08 +0900

    [Fix] remove ```Shelf```
    Fix: 지금은 완벽하게 제어 안될듯

--- src/components/dashboard/Layout.tsx
+++ src/components/dashboard/Layout.tsx
@@ -6,7 +6,10 @@ import { matchPath, useLocation } from "react-router-dom";
 
 export default function Layout({ children }: { children: ReactNode }) {
   const location = useLocation();
-  const headerExceptions = ["/dashboard/records/:recordId"];
+  const headerExceptions = [
+    "/dashboard/records/:recordId",
+    "/dashboard/centers",
+  ];
   const exceptions = ["/dashboard/records/post"];
   const isHeaderException = [...headerExceptions, ...exceptions].some((path) =>
     matchPath({ path }, location.pathname)
@@ -15,14 +18,12 @@ export default function Layout({ children }: { children: ReactNode }) {
     matchPath({ path }, location.pathname)
   );
   const container = {
-    displays: "flex gap-x-3.5",
-    boundaries: "p-5",
-    sizes: "h-screen overflow-y-auto",
+    displays: "flex gap-x-3.5 justify-center items-center",
     backgrounds: "bg-[#FAFCFF]",
+    boundaries: " pb-[156px]",
   };
   const body = {
-    displays: "flex flex-col",
-    sizes: "w-full",
+    displays: "flex flex-col ",
   };
   return (
     <div>
--- src/components/dashboard/record/PageById.layout.tsx
+++ src/components/dashboard/record/PageById.layout.tsx
@@ -1,13 +1,12 @@
 import HeaderDetail from "@/design/HeaderDetail";
 import { cn } from "fast-jsx/util";
-
 export default function RecordLayout({
   children,
 }: {
   children: React.ReactNode;
 }) {
   const container = {
-    positions: "relative",
+    positions: "relative z",
     displays: "flex flex-col",
     boundaries: "pt-[68px]",
     sizes: "w-full min-h-screen ",
@@ -15,9 +14,9 @@ export default function RecordLayout({
   const body = {
     displays: "flex flex-col flex-grow items-center justify-center",
   };
+
   return (
     <div className={cn(container)}>
-      <HeaderDetail title="작업필요" />
       <div className={cn(body)}>{children}</div>
     </div>
   );
