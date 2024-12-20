8687a07f2d91b1cf94ae3c342726706c590398d3
minhyeong <minhyeong.dev@gmail.com>
2024-12-21 00:35:33 +0900

    [Feat] center assets

---
+++ src/components/dashboard/center/molecule/Center.molecule.tsx
@@ -0,0 +1,34 @@
+import Svg from "@/asset/Svg";
+import { Center } from "@/interface/Center";
+import { cn } from "fast-jsx/util";
+
+export default function CenterMolecule({ name, location, tags }: Center) {
+  const container = {
+    displays: "flex flex-col gap-y-4",
+    sizes: "w-[353px] h-[280px] ",
+  };
+  return (
+    <div className={cn(container)}>
+      <div className="h-[163px]">
+        <div className="h-[163px] w-[290px] bg-black-5" />
+      </div>
+      <div className="flex flex-col">
+        <div className="flex justify-between items-center">
+          <div className="text-lg font-bold truncate">{name}</div>
+          <Svg.Icon.Heart />
+        </div>
+        <div className="text-sm">{location}</div>
+        <div className="flex flex-wrap gap-[6px] mt-5">
+          {tags.map((tag) => (
+            <div
+              key={tag}
+              className="text-sm leading-none px-[15px] py-[6px] flex w-auto bg-blue-2 text-blue-5 rounded-[5px]"
+            >
+              {tag}
+            </div>
+          ))}
+        </div>
+      </div>
+    </div>
+  );
+}
--- src/components/dashboard/center/organism/Centers.organism.tsx
+++ src/components/dashboard/center/organism/Centers.organism.tsx
@@ -1,15 +1,22 @@
+import useCenter from "@/hook/useCenter";
 import { cn } from "fast-jsx/util";
+import CenterMolecule from "../molecule/Center.molecule";
 
 export default function CentersOrganism() {
+  const { centers } = useCenter();
   const container = {
-    displays: "flex flex-col",
+    displays: "flex flex-col items-center gap-y-10",
     pressures: "pt-5 pl-5",
-    sizes: "h-full min",
+    sizes: "h-[calc(100vh-417px)] w-full",
     backgrounds: "bg-white",
+    styles: "overflow-y-scroll",
   };
+
   return (
     <div className={cn(container)}>
-      <h1>Centers</h1>
+      {centers?.map((center) => (
+        <CenterMolecule {...center} />
+      ))}
     </div>
   );
 }
