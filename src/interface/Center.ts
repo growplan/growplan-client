21138f378f95935d8f3e697b94f2adb915ede62a
minhyeong <minhyeong.dev@gmail.com>
2024-12-21 00:40:54 +0900

    [Feat] create Center interface

---
+++ src/interface/Center.ts
@@ -0,0 +1,23 @@
+type Tag =
+  | "언어"
+  | "놀이"
+  | "심리"
+  | "감각통합"
+  | "인지"
+  | "미술"
+  | "사회성"
+  | "특수체육"
+  | "ABA"
+  | "운동"
+  | "음악"
+  | "작업"
+  | "행동";
+
+export interface Center {
+  centerId: number;
+  images: string[];
+  isScraped: boolean;
+  location: string;
+  name: string;
+  tags: Tag[];
+}
