39a9c67c4e5651f2f6ed082d9cb1a3765bd2e248
minhyeong <minhyeong.dev@gmail.com>
2024-12-21 09:32:43 +0900

    [Feat] create ```useDevelopmentByType```

--- src/hook/useDevelopment.ts
+++ src/hook/useDevelopment.ts
@@ -2,6 +2,7 @@ import { userApi } from "@/connection";
 import { useQuery } from "react-query";
 import useSign from "./useSign";
 import useChild from "./useChild";
+import { DevelopmentType } from "@/interface/Development";
 
 export default function useDevelopment() {
   const { sign } = useSign();
@@ -13,4 +14,23 @@ export default function useDevelopment() {
       userApi.child.development.get(Number(sign?.id), Number(nowChild?.id)),
   });
   return { developmentInfo, isLoading };
+}
+
+export function useDevelopmentByType(developmentType: DevelopmentType) {
+  const { sign } = useSign();
+  const { nowChild } = useChild();
+  const { data: development, isLoading } = useQuery({
+    enabled: !!developmentType && !!sign && !!nowChild,
+    queryKey: ["getDevelopments", nowChild],
+    queryFn: () =>
+      userApi.child.development.getByType(
+        Number(sign?.id),
+        Number(nowChild?.id),
+        developmentType
+      ),
+  });
+  return {
+    development,
+    isLoading,
+  };
 }
